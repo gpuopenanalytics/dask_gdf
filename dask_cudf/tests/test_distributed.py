91f55fa21fe52d08653698e0fc66e63e91ac8121
Matthew Rocklin <mrocklin@gmail.com>
2019-01-08 11:30:07 -0800

    Remove string from data and include delayed check

--- dask_cudf/tests/test_distributed.py
+++ dask_cudf/tests/test_distributed.py
@@ -9,9 +9,12 @@ from distributed.utils_test import loop  # noqa: F401
 dask_cuda = pytest.importorskip("dask_cuda")
 
 
-def test_basic(loop):  # noqa: F811
+@pytest.mark.parametrize("delayed", [True, False])  # noqa: F811
+def test_basic(loop, delayed):  # noqa: F811
     with dask_cuda.LocalCUDACluster(loop=loop) as cluster:
         with Client(cluster):
-            pdf = dask.datasets.timeseries()
+            pdf = dask.datasets.timeseries(dtypes={"x": int}).reset_index()
             gdf = pdf.map_partitions(cudf.DataFrame.from_pandas)
+            if delayed:
+                gdf = dd.from_delayed(gdf.to_delayed())
             dd.assert_eq(pdf.head(), gdf.head())
