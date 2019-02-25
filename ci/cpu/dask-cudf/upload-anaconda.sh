6067aeaf749a88fc7622cadad6d648b39791181e
Benjamin Zaitlen <quasiben@gmail.com>
2019-02-25 08:17:49 -0800

    forgot to add ci folder and revert test

---
+++ ci/cpu/dask-cudf/upload-anaconda.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Adopted from https://github.com/tmcdonell/travis-scripts/blob/dfaac280ac2082cd6bcaba3217428347899f2975/update-accelerate-buildbot.sh
+export UPLOADFILE=`conda build conda-recipes -c rapidsai -c numba -c conda-forge -c defaults --python $PYTHON --output`
+
+set -e
+
+SOURCE_BRANCH=master
+
+test -e ${UPLOADFILE}
+
+LABEL_OPTION="--label main"
+
+# Restrict uploads to master branch
+  if [ ${GIT_BRANCH} != ${SOURCE_BRANCH} ]; then
+    echo "Skipping upload"
+    return 0
+  fi
+
+  if [ -z "$MY_UPLOAD_KEY" ]; then
+    echo "No upload key"
+    return 0
+  fi
+
+  echo "LABEL_OPTION=${LABEL_OPTION}"
+
+  echo "Upload"
+  echo ${UPLOADFILE}
+  anaconda -t ${MY_UPLOAD_KEY} upload -u rapidsai ${LABEL_OPTION} --force ${UPLOADFILE}
+fi
+
+echo "Upload"
+echo ${UPLOADFILE}
+anaconda -t ${MY_UPLOAD_KEY} upload -u rapidsai -l dev --force ${UPLOADFILE}
