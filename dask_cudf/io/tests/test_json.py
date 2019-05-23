f975deabbe47e5072adfc62ce6fafe6ecc6e0a33
rjzamora <rzamora@nvidia.com>
2019-05-23 07:39:49 -0700

    test cleanup

--- dask_cudf/io/tests/test_json.py
+++ dask_cudf/io/tests/test_json.py
@@ -8,19 +8,17 @@ import pytest
 
 
 def test_read_json(tmp_path):
-    df = dask.datasets.timeseries(
+    df1 = dask.datasets.timeseries(
         dtypes={"x": int, "y": int}, freq="120s").reset_index(drop=True)
-    df.to_json(tmp_path / "data-*.json")
+    df1.to_json(tmp_path / "data-*.json")
     df2 = dask_cudf.read_json(tmp_path / "data-*.json")
-    dd.assert_eq(df, df2)
+    dd.assert_eq(df1, df2)
 
 
 @pytest.mark.filterwarnings("ignore:Using CPU")
-@pytest.mark.parametrize('orient', ['split', 'records', 'index', 'columns',
-                                    'values'])
+@pytest.mark.parametrize('orient', ['split', 'index', 'columns', 'values'])
 def test_read_json_basic(orient):
-    df = pd.DataFrame({'x': ['a', 'b', 'c', 'd'],
-                       'y': [1, 2, 3, 4]})
+    df = pd.DataFrame({'x': ['a', 'b', 'c', 'd'], 'y': [1, 2, 3, 4]})
     ddf = dd.from_pandas(df, npartitions=2)
     with tmpfile('json') as f:
         df.to_json(f, orient=orient, lines=False)
@@ -31,4 +29,19 @@ def test_read_json_basic(orient):
         dd.assert_eq(out, actual_pd)
         if orient == 'values':
             out.columns = list(df.columns)
+        dd.assert_eq(out, df)
+
+
+@pytest.mark.filterwarnings("ignore:Using CPU")
+@pytest.mark.parametrize('lines', [True, False])
+def test_read_json_lines(lines):
+    df = pd.DataFrame({'x': ['a', 'b', 'c', 'd'], 'y': [1, 2, 3, 4]})
+    ddf = dd.from_pandas(df, npartitions=2)
+    with tmpfile('json') as f:
+        df.to_json(f, orient='records', lines=lines)
+        actual = dask_cudf.read_json(f, orient='records', lines=lines)
+        actual_pd = pd.read_json(f, orient='records', lines=lines)
+
+        out = actual.compute()
+        dd.assert_eq(out, actual_pd)
         dd.assert_eq(out, df)
